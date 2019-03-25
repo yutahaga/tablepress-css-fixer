8614e184643edcad78b5751d2e99819e72dce174
Yuta Haga <yutahaga7@gmail.com>
2019-03-25 17:40:37 +0900

    feat: ユーザビリティ向上

--- tablepress-css-fixer.php
+++ tablepress-css-fixer.php
@@ -14,9 +14,19 @@
 function tablepress_css_fixer_replace_script( $name, $dependencies = [] ) {
 	$suffix = SCRIPT_DEBUG ? '' : '.min';
 	$js_file = "admin/js/{$name}{$suffix}.js";
+	$css_file = "admin/js/${name}.css";
 	$js_url = plugins_url( $js_file, __FILE__ );
+	$css_url = plugins_url( $css_file, __FILE__ );
+	$can_fullcontrol = current_user_can( 'switch_themes' );
+	wp_localize_script( "tablepress-{$name}", 'can_fullcontrol', $can_fullcontrol );
 	wp_dequeue_script( "tablepress-{$name}" );
 	wp_enqueue_script( "tablepress-{$name}", $js_url, $dependencies, TablePress::version, true );
+	wp_enqueue_script("tablepress-polyfill", "https://polyfill.io/v3/polyfill.min.js?features=URL
+", [], null, true);
+
+	if (!$can_fullcontrol && isset($_GET['table_id']) && $_GET['table_id'] === '1') {
+		wp_enqueue_style( 'tablepress-css-fixer-style', $css_url, [], null );
+	}
 }
 
 add_action(
@@ -25,7 +35,7 @@ add_action(
 			return;
 		}
 
-		tablepress_css_fixer_replace_script( 'edit', [ 'jquery-core', 'jquery-ui-sortable', 'json2' ] );
+		tablepress_css_fixer_replace_script( 'edit', [ 'jquery-core', 'jquery-ui-sortable', 'json2', 'tablepress-polyfill' ] );
 	}, 10
 );
 
